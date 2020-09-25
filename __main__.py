b5d30accc5352652b998bc69b0383b7503b75b7a
ssss-ai <71875939+ssss-ai@users.noreply.github.com>
2020-09-25 05:25:35 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 
 
-token = ""
+token = "100,000"
 prefix = "plsstart!"
 
 bot = commands.Bot(command_prefix=prefix, self_bot=True)
